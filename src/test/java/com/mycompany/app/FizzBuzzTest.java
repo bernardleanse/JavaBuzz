0b5137ac98d40b8f07e257d91401eb21028a7449
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-29 08:46:20 +0100

    tests

--- src/test/java/com/mycompany/app/FizzBuzzTest.java
+++ src/test/java/com/mycompany/app/FizzBuzzTest.java
@@ -8,11 +8,22 @@ public class FizzBuzzTest {
   FizzBuzz fb = new FizzBuzz();
 
   @Test
-
   public void returnsFizzIfNumberDivisibleByThree(){
 
     assertEquals("fizz", fb.fizzBuzz(3));
 
+  }
+
+  @Test
+  public void returnBuzzIfNumberDivisibleByFive(){
+
+    assertEquals("buzz", fb.fizzBuzz(5));
+
+  }
+
+  @Test
+  public void returnFizzBuzzIfBoth(){
+    assertEquals("fizzbuzz", fb.fizzBuzz(15));
   }
 
 }
