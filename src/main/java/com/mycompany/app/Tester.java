1f771be4db4658a739e4847415e51ea331a648cc
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-29 08:45:35 +0100

    tester class for testing code statements manually

---
+++ src/main/java/com/mycompany/app/Tester.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+import com.mycompany.app.FizzBuzz;
+
+public class Tester {
+  FizzBuzz fb = new FizzBuzz();
+  public static void main(String args[]){
+    System.out.print("success");
+  }
+}
