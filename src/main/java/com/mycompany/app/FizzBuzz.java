8b78a15c3b9ef1bdbad84c3eadcd13906300d814
Bernard Leanse <bernardleanse96@gmail.com>
2022-03-29 08:46:08 +0100

    basic fizzbuzz implemetation

--- src/main/java/com/mycompany/app/FizzBuzz.java
+++ src/main/java/com/mycompany/app/FizzBuzz.java
@@ -3,7 +3,15 @@ package com.mycompany.app;
 public class FizzBuzz {
 
   public String fizzBuzz(int num){
-    return "fizz";
+    if(num % 3 == 0 && num % 5 == 0){
+      return "fizzbuzz";
+    } else if(num % 3 == 0) {
+      return "fizz";
+    } else if(num % 5 == 0){
+      return "buzz";
+    } else {
+      return String.valueOf(num);
+    }
   }
 
 }
